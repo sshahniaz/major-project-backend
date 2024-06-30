62399f920e22d5832e8131bf3b7ffcf1942992dc
sshahniaz <100251uk@saeinstitute.edu>
2024-06-30 15:20:39 +0100

    api working
    added the cleaned test set and perffromed data preprocessing in the api

--- ml_project/src/app.py
+++ ml_project/src/app.py
@@ -4,14 +4,15 @@ import json
 import csv
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-
-from sklearn.model_selection import load_model 
+import pickle 
 
 app = flask.Flask(__name__)
 
 # Load your pre-trained ML model (replace with your loading logic)
-model = load_model("./ml_project/models/BigMart_Sales_Model.pkl")  # Adjust the filename as needed
+with open("../models/BigMart_Sales_Model.pkl", "rb") as file:
+    model = pickle.load(file) # Adjust the filename as needed
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -32,11 +33,38 @@ def predict():
     except Exception as e:
         return flask.jsonify({"error": f"Error reading CSV file: {str(e)}"}), 400
 
-    # Extract features from the DataFrame (replace with your logic)
-    features = df  # Assuming all columns are features (adjust as needed)
+    categorical_col = ['ProductID', 'FatContent', 'ProductType', 'OutletID', 'OutletSize', 'LocationType', 'OutletType']
+    numerical_col = ['Weight', 'ProductVisibility', 'MRP', 'EstablishmentYear', 'OutletSales']
 
-    # Check if required features are present (optional but recommended)
-    # Assuming no specific features are required for the model
+
+    # Check if the CSV data has the expected columns
+    expected_columns = ['ProductID', 'FatContent', 'ProductType', 'OutletID', 'OutletSize', 'LocationType', 'OutletType', 'Weight', 'ProductVisibility', 'MRP', 'EstablishmentYear']
+    missing_columns = set(expected_columns) - set(df.columns)
+    if missing_columns:
+      return flask.jsonify({"error": f"Missing columns in CSV file: {', '.join(missing_columns)}"}), 400
+    
+    #Standardasing the FatContent column
+    ifc_dict = {'Low Fat':'Low Fat', 'Regular':'Regular', 'LF':'Low Fat', 'reg':'Regular', 'low fat':'Low Fat'}
+    df['FatContent'] = df['FatContent'].replace(ifc_dict)
+
+    #Encoding data
+    encode_dict = {'Low Fat':0, 'Regular': 1, 'Small':0, 'Medium':1,'High':2, 'Tier 3':2, 'Tier 2': 1, 'Tier 1': 0, 'Grocery Store':0, 'Supermarket Type1':1, 'Supermarket Type2':2, 'Supermarket Type3':3}
+    df = df.replace(encode_dict)
+
+    
+
+    categorical_columns = df.select_dtypes(include=['object']).columns
+
+    label_encoders = {}
+    for col in categorical_columns:
+      le = LabelEncoder()
+      df[col] = le.fit_transform(df[col].astype(str))
+      label_encoders[col] = le
+
+    features = df.drop(columns = ['Weight', 'FatContent','ProductID','OutletID'], axis = 1)  
+    scaler = StandardScaler()
+    features = scaler.fit_transform(features)
+
 
     # Make the prediction using your pre-trained Lasso model
     prediction = model.predict(features)
