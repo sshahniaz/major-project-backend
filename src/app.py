053a62c27a5a6f2c5b4b4a847a1df5ff220baad1
sshahniaz <100251uk@saeinstitute.edu>
2024-07-24 18:22:15 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -188,7 +188,7 @@ def predict():
     # Prepare the JSON response with the prediction
     # response = json.dumps(data, indent=4)  # Convert to list for JSON
     response = flask.jsonify(data)
-    response.headers['Access-Control-Allow-Origin'] = '*.vercel.app'
+    response.headers['Access-Control-Allow-Origin'] = '*'
 
     return response
 
